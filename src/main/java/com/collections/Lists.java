b2a455549c6fef4f70abc571f9a78c88a516a416
ManoloGioino <jmgioino@mail.austral.edu.ar>
2024-08-29 15:34:18 -0300

    entrega

--- src/main/java/com/collections/Lists.java
+++ src/main/java/com/collections/Lists.java
@@ -1,7 +1,59 @@
 package com.collections;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Lists {
+    public static Integer indexOf(String color, List<String> colors) {
+        int index = 0;
+        for (String current : colors) {
+            if (Objects.equals(current, color)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
 
+    public static Integer indexOfByIndex(String color, List<String> colors, Integer startIndex) {
+        for (int i = startIndex; i < colors.size(); i++) {
+            if (Objects.equals(colors.get(i), color)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static Integer indexOfEmpty(List<String> colors) {
+        int i = 0;
+        while (i < colors.size()) {
+            if (Objects.equals(colors.get(i), "")) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    public static Integer put(String color, List<String> colors) {
+        for (int i = 0; i < colors.size(); i++) {
+            if (colors.get(i).isEmpty()) {
+                colors.set(i, color); // Use set to replace the empty spot
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static Integer remove(String color, List<String> colors) {
+        int count = 0;
+        for (int i = 0; i < colors.size(); i++) {
+            if (Objects.equals(colors.get(i), color)) {
+                colors.remove(i);
+                i--; // Adjust the index after removal to avoid skipping elements
+                count++;
+            }
+        }
+        return count;
+    }
 }
--- src/main/java/com/collections/Sets.java
+++ src/main/java/com/collections/Sets.java
@@ -1,13 +1,26 @@
 package com.collections;
+
 import java.util.HashSet;
-import java.util.Set;
 import java.util.List;
+import java.util.Set;
 import java.util.Arrays;
 
 public class Sets {
 
-    private static final Set<String> ALCOHOLS = new HashSet<>(Arrays.asList("whiskey", "whisky", "white rum", "dark rum", "bourbon", "rye", "scotch", "vodka",
-            "tequila", "gin", "dry vermouth", "sweet vermouth", "prosecco","aperol", "brandy", "mezcal",
-            "triple sec", "coffee liqueur", "almond liqueur", "champagne", "orange curacao", "rum"));
+    // A set of common alcoholic ingredients
+    private static final Set<String> ALCOHOLIC_INGREDIENTS = new HashSet<>(Arrays.asList(
+            "whiskey", "whisky", "white rum", "dark rum", "bourbon", "rye", "scotch", "vodka",
+            "tequila", "gin", "dry vermouth", "sweet vermouth", "prosecco", "aperol", "brandy", "mezcal",
+            "triple sec", "coffee liqueur", "almond liqueur", "champagne", "orange curacao", "rum"
+    ));
 
+    // Method to check if a drink is a cocktail or mocktail
+    public static String checkDrinks(String drinkName, List<String> ingredients) {
+        for (String ingredient : ingredients) {
+            if (ALCOHOLIC_INGREDIENTS.contains(ingredient)) {
+                return drinkName + " Cocktail";
+            }
+        }
+        return drinkName + " Mocktail";
+    }
 }
